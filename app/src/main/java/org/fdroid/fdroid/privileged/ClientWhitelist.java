80452dec2410977ce61efa4b889378bc020afc88
Dominik Schürmann <dominik@dominikschuermann.de>
2016-08-02 00:20:31 +0200

    Replace signature permission with whitelist
    Instead of using a signature permission to protect
    the service, we now use a whitelist approach to
    ease the integration of the extension into custom roms.
    Now rom maintainer can build the extension on their
    own and sign it with their own key.
    The extension then still works with a F-Droid client
    signed with the F-Droid key.

---
+++ app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2016 Dominik Schürmann <dominik@dominikschuermann.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fdroid.fdroid.privileged;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Only apps signed using a certificate with a SHA-256 hash listed here
+ * can access the Privileged Extension!
+ * <p/>
+ * Get SHA-256 of certificate with:
+ * keytool -list -printcert -jarfile com.example.apk
+ */
+public class ClientWhitelist {
+
+    public static HashSet<String> whitelist = new HashSet<>(Arrays.asList(
+            // certificate SHA-256 of https://f-droid.org/FDroid.apk
+            "43:23:8D:51:2C:1E:5E:B2:D6:56:9F:4A:3A:FB:F5:52:34:18:B8:2E:0A:3E:D1:55:27:70:AB:B9:A9:C9:CC:AB"
+    ));
+
+}
