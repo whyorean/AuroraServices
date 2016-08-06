e3cf082ffb28c8c370636a6031ef34a3552cce35
relan <email@hidden>
2016-08-06 08:13:46 +0300

    Add Android.mk
    Required for integration into ROMs.

---
+++ app/src/main/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := FDroidPrivilegedExtension
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SDK_VERSION := current
+LOCAL_SRC_FILES := $(call all-java-files-under, java) \
+                   $(call all-Iaidl-files-under, aidl)
+LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/aidl
+
+include $(BUILD_PACKAGE)
