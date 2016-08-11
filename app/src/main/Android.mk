5c58c3e32840ac1e1c70be2be814c9eaa1d8cdd2
relan <email@hidden>
2016-08-11 22:04:26 +0300

    Disable ProGuard
    Avoid IPackageInstallObserver and IPackageDeleteObserver removal.

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -5,6 +5,8 @@ LOCAL_PACKAGE_NAME := FDroidPrivilegedExtension
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SDK_VERSION := current
+# Keep IPackageInstallObserver and IPackageDeleteObserver
+LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_SRC_FILES := $(call all-java-files-under, java) \
                    $(call all-Iaidl-files-under, aidl)
 LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/aidl
