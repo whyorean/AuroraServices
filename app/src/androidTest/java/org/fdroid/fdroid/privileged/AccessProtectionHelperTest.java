83b04dac6d69ab45e340585a0e05613caabe56d1
Dominik Schürmann <dominik@dominikschuermann.de>
2016-08-11 18:29:28 +0200

    Include packageName in whitelist
    Fixes #6

--- app/src/androidTest/java/org/fdroid/fdroid/privileged/AccessProtectionHelperTest.java
+++ app/src/androidTest/java/org/fdroid/fdroid/privileged/AccessProtectionHelperTest.java
@@ -19,6 +19,7 @@ package org.fdroid.fdroid.privileged;
 import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Pair;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -33,11 +34,16 @@ import static junit.framework.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class AccessProtectionHelperTest {
 
-    public static HashSet<String> testWhitelist = new HashSet<>(Arrays.asList(
+    public static HashSet<Pair<String, String>> testCorrectWhitelist = new HashSet<>(Arrays.asList(
             // Android System Apps:
-            "c8a2e9bccf597c2fb6dc66bee293fc13f2fc47ec77bc6b2b0d52c11f51192ab8",
+            new Pair<>("android", "c8a2e9bccf597c2fb6dc66bee293fc13f2fc47ec77bc6b2b0d52c11f51192ab8"),
             // Android included Apps:
-            "a40da80a59d170caa950cf15c18c454d47a39b26989d8b640ecd745ba71bf5dc"
+            new Pair<>("com.android.camera", "a40da80a59d170caa950cf15c18c454d47a39b26989d8b640ecd745ba71bf5dc")
+    ));
+
+    public static HashSet<Pair<String, String>> testWrongWhitelist = new HashSet<>(Arrays.asList(
+            // wrong package name
+            new Pair<>("NOPE", "c8a2e9bccf597c2fb6dc66bee293fc13f2fc47ec77bc6b2b0d52c11f51192ab8")
     ));
 
     Instrumentation instrumentation;
@@ -50,7 +56,7 @@ public class AccessProtectionHelperTest {
     @Test
     public void testAccessAllowed() {
         AccessProtectionHelper accessProtectionHelper =
-                new AccessProtectionHelper(instrumentation.getContext(), testWhitelist);
+                new AccessProtectionHelper(instrumentation.getContext(), testCorrectWhitelist);
 
         // system app:
         assertTrue(accessProtectionHelper.isPackageAllowed("android"));
@@ -63,6 +69,18 @@ public class AccessProtectionHelperTest {
         // uses normal whitelist
         AccessProtectionHelper accessProtectionHelper =
                 new AccessProtectionHelper(instrumentation.getContext());
+
+        // system app:
+        assertFalse(accessProtectionHelper.isPackageAllowed("android"));
+        // included app:
+        assertFalse(accessProtectionHelper.isPackageAllowed("com.android.camera"));
+    }
+
+    @Test
+    public void testAccessWrongWhitelist() {
+        // uses normal whitelist
+        AccessProtectionHelper accessProtectionHelper =
+                new AccessProtectionHelper(instrumentation.getContext(), testWrongWhitelist);
 
         // system app:
         assertFalse(accessProtectionHelper.isPackageAllowed("android"));
--- app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
+++ app/src/main/java/org/fdroid/fdroid/privileged/ClientWhitelist.java
@@ -16,6 +16,8 @@
 
 package org.fdroid.fdroid.privileged;
 
+import android.util.Pair;
+
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -28,9 +30,9 @@ import java.util.HashSet;
  */
 public class ClientWhitelist {
 
-    public static HashSet<String> whitelist = new HashSet<>(Arrays.asList(
+    public static HashSet<Pair<String, String>> whitelist = new HashSet<>(Arrays.asList(
             // certificate SHA-256 of https//f-droid.org/FDroid.apk
-            "43238d512c1e5eb2d6569f4a3afbf5523418b82e0a3ed1552770abb9a9c9ccab"
+            new Pair<>("org.fdroid.fdroid", "43238d512c1e5eb2d6569f4a3afbf5523418b82e0a3ed1552770abb9a9c9ccab")
     ));
 
 }
