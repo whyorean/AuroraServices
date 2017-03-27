9d7cb532fe911b791c2fd9e017b960ecb3eac1d9
Chirayu Desai <chirayudesai1@gmail.com>
2017-03-27 21:55:28 +0530

    Add script to persist update.zip installed files across system updates
    * The files installed by the update.zip are lost on a system
      update.
    * However, most third party Android distributions include a
      mechanism to backup files before the update, and restore
      them afterwards, which is handled by scripts in
      /system/addon.d

---
+++ app/src/main/scripts/80-fdroid.sh
@@ -0,0 +1,44 @@
+#!/sbin/sh
+#
+# /system/addon.d/80-fdroid.sh
+# During a system upgrade, this script backs up F-Droid client and privext,
+# /system is formatted and reinstalled, then the files are restored.
+#
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+app/FDroid.apk
+app/FDroid/FDroid.apk
+priv-app/FDroidPrivilegedExtension
+priv-app/FDroidPrivilegedExtension/FDroidPrivilegedExtension.apk
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/"$FILE"
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
